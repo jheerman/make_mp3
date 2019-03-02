5d2c2c0815bd18ea5330f512d891ed763f0f75a7
John Heerman <jheerman@gmail.com>
2019-03-01 22:30:17 -0600

    Add shebang and comments

--- do_mp3.sh
+++ do_mp3.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+##########################################
+# Iterate over files in a directory
+# looking for flac files and
+# converting them to mp3 files
+# while copying the metadata
+##########################################
 for f in *.flac; 
   do 
     output=$(echo "$f" | sed -e 's/flac/mp3/g'); 
