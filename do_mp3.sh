8e12d45bd5ba1453137d742056433f9b883c883c
John Heerman <jheerman@gmail.com>
2020-05-28 21:23:17 -0500

    Add support for a maxdepth of 1 directory level

--- do_mp3.sh
+++ do_mp3.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-##########################################
-# Iterate over files in a directory
-# looking for flac files and
-# converting them to mp3 files
-# while copying the metadata
-##########################################
-for f in *.flac; 
-  do 
-    output=$(echo "$f" | sed -e 's/flac/mp3/g'); 
-    ffmpeg -i "$f" -ab 320k -map_metadata 0 -id3v2_version 3 "$output"; 
-  done
+##################################################################
+# Iterate over files in a directory looking for flac files and
+# converting them to mp3 files while copying the metadata
+# 5/27/20 - Now supports iterating over subdirectories in the root
+##################################################################
+
+echo "Looking for subdirectories..."
+for d in *; do
+    if [ -d "$d" ]; then
+        echo "Looking for flac files in $d"
+        for f in $d/*.flac; do
+            echo "Converting file $f"
+            output=$(echo "$f" | sed -e 's/flac/mp3/g');
+            ffmpeg -v quiet -i "$f" -ab 320k -map_metadata 0 -id3v2_version 3 "$output";                              
+        done
+    fi
+done
+
+echo "Looking for flacs in current directory..."
+for f in *.flac;
+    do
+        output=$(echo "$f" | sed -e 's/flac/mp3/g');
+        ffmpeg -i "$f" -ab 320k -map_metadata 0 -id3v2_version 3 "$output";                                           
+    done
+
+echo "Done."
